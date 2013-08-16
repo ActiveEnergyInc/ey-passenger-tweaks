459fec49a6e199898977c6dca85918195d9dd728
Zaid Al-Jarrah <zaid.aljarrah@activeenergy.ca>
2013-08-16 17:18:13 -0400

    Modify Passenger settings for all apps

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,3 @@
-# Add your appname here
-app_name = node['name']
 # Add the new worker count you want
 worker_count = 6
 # Add the memory limit you want to pass
@@ -7,27 +5,29 @@ worker_count = 6
 memory_limit = 600
 
 if ['app_master', 'app', 'solo'].include?(node[:instance_role])
-  ey_cloud_report "passenger" do
-    message "changing passenger settings"
-  end
+  node[:applications].each do |app_name, data|
+    ey_cloud_report "passenger" do
+      message "changing passenger settings"
+    end
 
-  service "nginx" do
-    supports :restart => true
-    action :enable
-  end
-  
-  execute "Alter passenger_max_pool_size" do
-    command "sed -i -r 's/passenger_max_pool_size [0-9]+;/passenger_max_pool_size #{worker_count};/' /etc/nginx/stack.conf"
-    notifies :restart, resources(:service => 'nginx')
-    not_if "grep 'passenger_max_pool_size #{worker_count}' /etc/nginx/stack.conf'"
-  end
+    service "nginx" do
+      supports :restart => true
+      action :enable
+    end
 
-  cron "passenger_monitor_#{app_name}" do
-    minute  '*'
-    hour    '*'
-    day     '*'
-    month   '*'
-    weekday '*'
-    command "/usr/bin/lockrun --lockfile=/var/run/passenger_monitor_#{app_name}.lockrun -- /bin/bash -c '/engineyard/bin/passenger_monitor #{app_name} -l #{memory_limit} >/dev/null 2>&1'"
+    execute "Alter passenger_max_pool_size" do
+      command "sed -i -r 's/passenger_max_pool_size [0-9]+;/passenger_max_pool_size #{worker_count};/' /etc/nginx/stack.conf"
+      notifies :restart, resources(:service => 'nginx')
+      not_if "grep 'passenger_max_pool_size #{worker_count}' /etc/nginx/stack.conf'"
+    end
+
+    cron "passenger_monitor_#{app_name}" do
+      minute  '*'
+      hour    '*'
+      day     '*'
+      month   '*'
+      weekday '*'
+      command "/usr/bin/lockrun --lockfile=/var/run/passenger_monitor_#{app_name}.lockrun -- /bin/bash -c '/engineyard/bin/passenger_monitor #{app_name} -l #{memory_limit} >/dev/null 2>&1'"
+    end
   end
 end
